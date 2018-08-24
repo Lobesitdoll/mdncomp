ccd244874e021a955facc23d74cc67a722851425
epistemex <github@epistemex.com>
2018-08-24 09:02:58 -0500

    More specific node version check (8.3)

--- src/mdncomp.js
+++ src/mdncomp.js
@@ -12,7 +12,7 @@ const lf = "\r\n";
 
 // Internal texts needed in case errors happens before locale files are loaded. Are merged with locale below.
 const text = {
-  "versionWarning"  : "WARNING: mdncomp is built for Node version 8 or newer. It may not work in older Node.js versions.",
+  "versionWarning"  : "WARNING: mdncomp is built for Node version 8.3 or newer. It may not work in older Node.js versions.",
   "criticalDataFile": "Critical error: data file not found.\n?yTry running with option --fupdate to download latest snapshot.",
   "missingModule"   : "Critical: A core module seem to be missing. Use 'npm i -g mdncomp' to reinstall.",
   "unhandled"       : "------- An unhandled error occurred! -------\n\nConsider reporting to help us solve it via GitLab if it persists:\nhttps:\/\/gitlab.com/epistemex/mdncomp/issues\n\nAlternatively:\nTry update/reinstall 'npm i -g mdncomp' (or --fupdate for just data).\n\n"
@@ -20,7 +20,8 @@ const text = {
 
 /*- SYSTEM VALIDATIONS AND ERROR HANDLING ------------------------------------*/
 
-if ( +process.versions.node.split(".")[ 0 ] < 8 ) {
+const nv = process.versions.node.split(".").map(n => n|0);
+if ( nv[ 0 ] < 8 || (nv[ 0 ] === 8 && nv[ 1 ] < 3) ) {
   console.log(text.versionWarning);
 }
 
